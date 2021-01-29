a057272dc14d65e122cae420690ef35e530e490a
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 13:58:14 -0300

    remove elb

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,44 +122,44 @@ resource "aws_launch_configuration" "itau-test" {
 //  }
 //}
 
-resource "aws_elb" "elb1" {
-  name               = "terraform-elb"
-  availability_zones = "${var.availability_zones}"
-  security_groups    = ["${var.sg_loadbalancer}"]
+//resource "aws_elb" "elb1" {
+//  name               = "terraform-elb"
+//  availability_zones = "${var.availability_zones}"
+//  security_groups    = ["${var.sg_loadbalancer}"]
+//
+//  listener {
+//    instance_port     = 80
+//    instance_protocol = "http"
+//    lb_port           = 80
+//    lb_protocol       = "http"
+//  }
+//
+//  health_check {
+//    healthy_threshold   = 2
+//    unhealthy_threshold = 2
+//    timeout             = 3
+//    target              = "HTTP:80/"
+//    interval            = 30
+//  }
+//
+//  cross_zone_load_balancing   = true
+//  idle_timeout                = 400
+//  connection_draining         = true
+//  connection_draining_timeout = 400
+//
+//}
+//
+//resource "aws_lb_cookie_stickiness_policy" "cookie_stickness" {
+//  name                     = "cookiestickness"
+//  load_balancer            = "${aws_elb.elb1.id}"
+//  lb_port                  = 80
+//  cookie_expiration_period = 600
+//}
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-
-}
-
-resource "aws_lb_cookie_stickiness_policy" "cookie_stickness" {
-  name                     = "cookiestickness"
-  load_balancer            = "${aws_elb.elb1.id}"
-  lb_port                  = 80
-  cookie_expiration_period = 600
-}
-
-output "availabilityzones" {
-  value = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
-}
-
-output "elb-dns" {
-  value = "${aws_elb.elb1.dns_name}"
-}
+//output "availabilityzones" {
+//  value = "${var.availability_zones}"
+//}
+//
+//output "elb-dns" {
+//  value = "${aws_elb.elb1.dns_name}"
+//}
