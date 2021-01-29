0777abc66941f5097e77c4bae798f03a0af109a0
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 13:01:45 -0300

    uploadv2

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,20 +1,3 @@
-resource "aws_autoscaling_group" "scalegroup" {
-  launch_configuration = "${aws_launch_configuration.itau-test.name}"
-  availability_zones   = "${var.availability_zones}"
-  min_size             = REPLICAS
-  max_size             = REPLI_MAX
-  enabled_metrics      = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
-  metrics_granularity  = "1Minute"
-  load_balancers       = ["${aws_elb.elb1.id}"]
-  health_check_type    = "ELB"
-  
-  tag {
-    key                 = "Name"
-    value               = "webserver000"
-    propagate_at_launch = true
-  }
-}
-
 resource "aws_cloudformation_stack" "scalegroup" {
   name = "${var.cfn_stack_name}"
  
@@ -24,7 +7,7 @@ Resources:
   ASG:
     Type: AWS::AutoScaling::AutoScalingGroup
     Properties:
-      VPCZoneIdentifier: ["${join("\",\"", var.subnets)}"]
+      VPCZoneIdentifier: ["${join("\",\"", var.private-subnet)}"]
       AvailabilityZones: ["${join("\",\"", var.availability_zones)}"]
       LaunchConfigurationName: "${aws_launch_configuration.ecs.name}"
       MinSize: "${var.asg_min_size}"
@@ -36,7 +19,7 @@ Resources:
       AutoScalingCreationPolicy:
         MinSuccessfulInstancesPercent: 80
       ResourceSignal:
-        Count: "${var.cfn_signal_count}"
+        Count: "5"
         Timeout: PT10M
     UpdatePolicy:
     # Ignore differences in group size properties caused by scheduled actions
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,8 +34,48 @@ variable "asg_desired_capacity" {
   default = "2"
 }
 
-variable "cfn_stack_description" {
-  default = "itau-test"
+variable "private-subnet"{
+  default = ["subnet-0b7adb20d3d934993", "subnet-0ab18e2c4fce31174"]
 }
 
+variable "public-subnets" {
+  default = "subnet-0f85f00e46ac18e76"
+}
+
+variable "vpc_id" {
+  default = "vpc-06ff555b7f6cb853a"
+}
+
+variable "sg_loadbalancer" {
+ default = "sg-048c86c6342ca9942"
+}
+
+data "aws_ami" "ec2-linux" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+variable "instance_class" {
+  default     = "db.t2.micro"
+  description = "Instance class"
+}
+
+
+variable "cfn_stack_name" {
+  default     = "itau-test"
+}
+
+variable "cfn_stack_description" {
+  default     = "itau-test"
+}
 
