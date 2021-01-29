4a8f703c69a534391af119b993fa51657e4a81da
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 13:18:50 -0300

    remove scalealarm

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,49 +27,49 @@ resource "aws_launch_configuration" "itau-test" {
 //  autoscaling_group_name = "${aws_autoscaling_group.scalegroup.name}"
 //}
 
-resource "aws_cloudwatch_metric_alarm" "cpualarm" {
-  alarm_name          = "terraform-alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "60"
+//resource "aws_cloudwatch_metric_alarm" "cpualarm" {
+//  alarm_name          = "terraform-alarm"
+//  comparison_operator = "GreaterThanOrEqualToThreshold"
+//  evaluation_periods  = "2"
+//  metric_name         = "CPUUtilization"
+//  namespace           = "AWS/EC2"
+//  period              = "120"
+//  statistic           = "Average"
+//  threshold           = "60"
+//
+//  dimensions = {
+//    AutoScalingGroupName = "itau-test"
+//  }
+//
+//  alarm_description = "This metric monitor EC2 instance cpu utilization"
+//  alarm_actions     = ["${aws_autoscaling_policy.autopolicy.arn}"]
+//}
 
-  dimensions = {
-    AutoScalingGroupName = "itau-test"
-  }
+//resource "aws_autoscaling_policy" "autopolicy-down" {
+//  name                   = "terraform-autoplicy-down"
+//  scaling_adjustment     = -1
+//  adjustment_type        = "ChangeInCapacity"
+//  cooldown               = 300
+//  autoscaling_group_name = "${aws_autoscaling_group.scalegroup.name}"
+//}
 
-  alarm_description = "This metric monitor EC2 instance cpu utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.autopolicy.arn}"]
-}
-
-resource "aws_autoscaling_policy" "autopolicy-down" {
-  name                   = "terraform-autoplicy-down"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = "${aws_autoscaling_group.scalegroup.name}"
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpualarm-down" {
-  alarm_name          = "terraform-alarm-down"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "10"
-
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.scalegroup.name}"
-  }
-
-  alarm_description = "This metric monitor EC2 instance cpu utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.autopolicy-down.arn}"]
-}
+//resource "aws_cloudwatch_metric_alarm" "cpualarm-down" {
+//  alarm_name          = "terraform-alarm-down"
+//  comparison_operator = "LessThanOrEqualToThreshold"
+//  evaluation_periods  = "2"
+//  metric_name         = "CPUUtilization"
+//  namespace           = "AWS/EC2"
+//  period              = "120"
+//  statistic           = "Average"
+//  threshold           = "10"
+//
+//  dimensions = {
+//    AutoScalingGroupName = "${aws_autoscaling_group.scalegroup.name}"
+//  }
+//
+//  alarm_description = "This metric monitor EC2 instance cpu utilization"
+//  alarm_actions     = ["${aws_autoscaling_policy.autopolicy-down.arn}"]
+//}
 
 resource "aws_security_group" "websg" {
   name = "security_group_for_web_server"
