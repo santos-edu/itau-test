f40ea7f18d76a02e6f6a1f015f1232d568f00fed
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 12:07:53 -0300

    upload

---
+++ terraform/asg.tf
@@ -0,0 +1,60 @@
+
+resource "aws_autoscaling_group" "scalegroup" {
+  launch_configuration = "${aws_launch_configuration.itau-test.name}"
+  availability_zones   = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
+  min_size             = REPLICAS
+  max_size             = REPLI_MAX
+  enabled_metrics      = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
+  metrics_granularity  = "1Minute"
+  load_balancers       = ["${aws_elb.elb1.id}"]
+  health_check_type    = "ELB"
+  
+  tag {
+    key                 = "Name"
+    value               = "webserver000"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_cloudformation_stack" "scalegroup" {
+  name = "${var.cfn_stack_name}"
+ 
+  template_body = <<EOF
+Description: "${var.cfn_stack_description}"
+Resources:
+  ASG:
+    Type: AWS::AutoScaling::AutoScalingGroup
+    Properties:
+      VPCZoneIdentifier: ["${join("\",\"", var.subnets)}"]
+      AvailabilityZones: ["${join("\",\"", var.availability_zones)}"]
+      LaunchConfigurationName: "${aws_launch_configuration.ecs.name}"
+      MinSize: "${var.asg_min_size}"
+      MaxSize: "${var.asg_max_size}"
+      DesiredCapacity: "${var.asg_desired_capacity}"
+      HealthCheckType: EC2
+ 
+    CreationPolicy:
+      AutoScalingCreationPolicy:
+        MinSuccessfulInstancesPercent: 80
+      ResourceSignal:
+        Count: "${var.cfn_signal_count}"
+        Timeout: PT10M
+    UpdatePolicy:
+    # Ignore differences in group size properties caused by scheduled actions
+      AutoScalingScheduledAction:
+        IgnoreUnmodifiedGroupSizeProperties: true
+      AutoScalingRollingUpdate:
+        MaxBatchSize: "${var.asg_max_size}"
+        MinInstancesInService: "${var.asg_min_size}"
+        MinSuccessfulInstancesPercent: 80
+        PauseTime: PT10M
+        SuspendProcesses:
+          - HealthCheck
+          - ReplaceUnhealthy
+          - AZRebalance
+          - AlarmNotification
+          - ScheduledActions
+        WaitOnResourceSignals: true
+    DeletionPolicy: Retain
+  EOF
+}
--- user-data/bootstrap.sh
+++ user-data/bootstrap.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
+set -euo pipefail
 
 sudo yum install -y nginx
 sudo service nginx start
 sudo chkconfig nginx on
 
 echo "Hello world" > /usr/share/nginx/html/index.html
+
+<...>
+echo "Checking that agent is running"
+until $(curl --output /dev/null --silent --head --fail http://localhost:51678/v1/metadata); do
+  printf '.'
+  sleep 1
+done
+exit_code=$?
+printf "\nDone\n"
+
+# Can't signal back if the stack is in UPDATE_COMPLETE state, so ignore failures to do so.
+# CFN will roll back if it expects the signal but doesn't get it anyway.
+echo "Reporting $exit_code exit code to Cloudformation"
+/opt/aws/bin/cfn-signal \
+  --exit-code "$exit_code" \
+  --stack "$CFN_STACK" \
+  --resource ASG \
+  --region "$REGION" || true
