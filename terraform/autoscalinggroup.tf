e814919cf9b2c7839f859413e5a96833b29dce11
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 16:11:20 -0300

    img

--- terraform/autoscalinggroup.tf
+++ terraform/autoscalinggroup.tf
@@ -1,9 +1,9 @@
 resource "aws_autoscaling_group" "scalegroup" {
   launch_configuration = "${aws_launch_configuration.itau-test.name}"
   availability_zones   = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
-  min_size             = 2
+  min_size             = REPLICAS
   max_size             = REPLI_MAX
-  desired_capacity     = 2
+  desired_capacity     = REPLICAS_DESIRED
   enabled_metrics      = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
   metrics_granularity  = "1Minute"
   load_balancers       = ["${aws_elb.elb1.id}"]
