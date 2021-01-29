ed9b277e1ee06a3b1d61586c1e5345a3e47ba557
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 15:59:22 -0300

    img

--- user-data/bootstrap.sh
+++ user-data/bootstrap.sh
@@ -5,5 +5,5 @@ sudo service nginx start
 sudo chkconfig nginx on
 
 echo '''<h2 style="color: #2e6c80;"><img style="display: block; margin-left: auto; margin-right: auto;" src="https://raw.githubusercontent.com/santos-edu/itau-test/main/img/tweet-hunt.jpeg" alt="" width="300" height="169" /></h2>
-<h2 style="color: #2e6c80; text-align: center;">Itau-test V1</h2>
+<h2 style="color: #2e6c80; text-align: center;">Itau-test V3</h2>
 <p>&nbsp;</p>''' > /usr/share/nginx/html/index.html
