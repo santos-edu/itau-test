8823ce9f97e527be45af75f8d603fbcdca62e101
santos-edu <42344004+santos-edu@users.noreply.github.com>
2021-01-29 14:53:36 -0300

    Update autoscalinggroup.tf

--- terraform/autoscalinggroup.tf
+++ terraform/autoscalinggroup.tf
@@ -1,16 +1,23 @@
 resource "aws_autoscaling_group" "scalegroup" {
+  name = "${aws_launch_configuration.worker.name}-asg"
   launch_configuration = "${aws_launch_configuration.itau-test.name}"
   availability_zones   = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
-  min_size             = REPLICAS
-  max_size             = REPLI_MAX
+  min_size             = 2
+  max_size             = 6
+  desired_capacity     = 2
   enabled_metrics      = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupTotalInstances"]
   metrics_granularity  = "1Minute"
   load_balancers       = ["${aws_elb.elb1.id}"]
   health_check_type    = "ELB"
-  
+  launch_configuration = "${aws_launch_configuration.worker.name}"
+  vpc_zone_identifier  = ["${aws_subnet.public.*.id}"]
+
   tag {
     key                 = "Name"
     value               = "webserver000"
     propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
