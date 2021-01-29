3da7da98154f7bc2a7e7e2c5411f362e2e8dc9d5
Eduardo <eduardo.b.santos@hotmail.com>
2021-01-29 15:49:30 -0300

    img

--- user-data/bootstrap.sh
+++ user-data/bootstrap.sh
@@ -4,4 +4,6 @@ sudo yum install -y nginx
 sudo service nginx start
 sudo chkconfig nginx on
 
-echo "Hello world" > /usr/share/nginx/html/index.html
+echo '''<h2 style="color: #2e6c80;"><img style="display: block; margin-left: auto; margin-right: auto;" src="https://raw.githubusercontent.com/santos-edu/itau-test/main/img/tweet-hunt.jpeg" alt="" width="300" height="169" /></h2>
+<h2 style="color: #2e6c80; text-align: center;">Itau-test V1</h2>
+<p>&nbsp;</p>''' > /usr/share/nginx/html/index.html
